1144dd079648e2f1e8fd867ef0f6e9c63c17ae34
Script-Savant <alexkienjeku@gmail.com>
2025-06-12 23:42:22 +0300

    layouts

--- BlazorApp1/BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b087de7d1654578765c1b92fd9faf958df7676e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970efddfde1d18e9a8767124e50e84fbe99d5ab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
