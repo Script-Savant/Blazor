a44897a83994daeaacedb7b32dd457f90203b1eb
Script-Savant <alexkienjeku@gmail.com>
2025-06-13 22:14:36 +0300

    chapter 2

--- Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48780c5dc5a8d24fa646965feeeafee9f0b4b3ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a0904c18882555a67d332427c3296a5c84ff577")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
