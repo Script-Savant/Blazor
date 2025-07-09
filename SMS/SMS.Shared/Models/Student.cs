8db87bfb1fd67833cd082501cb60b4e038cb4c8f
Script-Savant <alexkienjeku@gmail.com>
2025-07-09 16:07:11 +0300

    updates

--- SMS/SMS.Shared/Models/Student.cs
+++ SMS/SMS.Shared/Models/Student.cs
@@ -21,6 +21,7 @@ public class Student
     public required string FirstName { get; set; }
     public required string MiddleName { get; set; }
     public required string LastName { get; set; }
+    public string FullName => $"{FirstName} {MiddleName} {LastName}".Trim();
     
     [EmailAddress]
     public required string EmailAddress { get; set; }
